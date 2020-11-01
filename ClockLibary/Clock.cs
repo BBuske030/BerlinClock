2e91bea0a68b6039728f2e5a8d564a1400167031
BBuske030 <buske-b@web.de>
2020-11-01 17:37:57 +0100

    Add logic for clock functions

--- ClockLibary/Clock.cs
+++ ClockLibary/Clock.cs
@@ -20,7 +20,18 @@ namespace ClockLibary
         /// <returns>A String with length(1) Y = Yellow; O = Off</returns>
         public string GetTwoSecoundsIndicatorString()
         {
-            return "Y";
+            string indicator = "O";
+
+            try
+            {
+                indicator = (currentTime.Seconds % 2) == 0 ? "Y" : "O";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return indicator;
         }
 
         /// <summary>
@@ -29,7 +40,24 @@ namespace ClockLibary
         /// <returns>A String with length(4) R = Red; O = Off</returns>
         public string GetFiveHoursIndicatorString()
         {
-            return "RROO";
+            string indicator = "";
+
+            try
+            {
+                int hours = currentTime.Hours;
+                hours = (int)Math.Floor((double)(hours * 5) / 24);
+                indicator = indicator.PadRight(hours, 'R');
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                indicator = indicator.PadRight(4, 'O');
+            }
+
+            return indicator;
         }
 
         /// <summary>
@@ -38,7 +66,24 @@ namespace ClockLibary
         /// <returns>A String with length(4) R = Red; O = Off</returns>
         public string GetOneHourIndicatorString()
         {
-            return "RRRO";
+            string indicator = "";
+
+            try
+            {
+                int hours = currentTime.Hours;
+                hours %= 5;
+                indicator = indicator.PadRight(hours, 'R');
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                indicator = indicator.PadRight(4, 'O');
+            }
+
+            return indicator;
         }
 
         /// <summary>
@@ -48,7 +93,34 @@ namespace ClockLibary
         /// <returns>A String with length(11) Y = Yellow; R = Red; O = Off</returns>
         public string GetFiveMinutesIndicatorString()
         {
-            return "YYROOOOOOOO";
+            string indicator = "";
+
+            try
+            {
+                int minutes = currentTime.Minutes;
+
+                for (int i = 5; i <= minutes; i += 5)
+                {
+                    if (i % 15 == 0)
+                    {
+                        indicator += "R";
+                    }
+                    else
+                    {
+                        indicator += "Y";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                indicator = indicator.PadRight(11, 'O');
+            }
+
+            return indicator;
         }
 
 
@@ -58,7 +130,24 @@ namespace ClockLibary
         /// <returns>A String with length(4) Y = Yellow; O = Off</returns>
         public string GetOneMinuteIndicatorString()
         {
-            return "YYOO";
+            string indicator = "";
+
+            try
+            {
+                int minutes = currentTime.Minutes;
+                minutes %= 5;
+                indicator = indicator.PadRight(minutes, 'Y');
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                indicator = indicator.PadRight(4, 'O');
+            }
+
+            return indicator;
         }
     }
 }
