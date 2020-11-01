e2ba7fa595347f17249060ac063598eedc77b277
BBuske030 <buske-b@web.de>
2020-11-01 17:48:44 +0100

    Add more tests for clock functions

--- ClockTests/TimeIndicatorStringTests.cs
+++ ClockTests/TimeIndicatorStringTests.cs
@@ -9,66 +9,108 @@ namespace ClockTests
         [Fact]
         public void TestTwoSecoundsIndicatorString()
         {
-            Clock clock = new Clock(TimeSpan.Parse("13:17:02"));
+            Clock clock = new Clock(TimeSpan.Parse("00:00:00"));
             String indicator = clock.GetTwoSecoundsIndicatorString();
 
             // Test output String length
             Assert.Equal(1, indicator.Length);
 
-            // Test for secound lamp indicator string (indicator on)
+            // Test for secound lamp indicator string (lamp on)
             Assert.Equal("Y", indicator);
+
+            // Test for secound lamp indicator string (lamp off)
+            clock = new Clock(TimeSpan.Parse("00:00:01"));
+            indicator = clock.GetTwoSecoundsIndicatorString();
+
+            Assert.Equal("O", indicator);
         }
 
         [Fact]
         public void TestFiveHoursIndicatorString()
         {
-            Clock clock = new Clock(TimeSpan.Parse("13:17:02"));
+            Clock clock = new Clock(TimeSpan.Parse("00:00:00"));
             String indicator = clock.GetFiveHoursIndicatorString();
 
             // Test output String length
             Assert.Equal(4, indicator.Length);
 
+            // Test for five hours indicator string (all lamps off)
+            Assert.Equal("OOOO", indicator);
+
+            clock = new Clock(TimeSpan.Parse("23:00:00"));
+            indicator = clock.GetFiveHoursIndicatorString();
+
+            // Test output String length (all lamps on)
+            Assert.Equal(4, indicator.Length);
+
             // Test for five hours indicator string
-            Assert.Equal("RROO", indicator);
+            Assert.Equal("RRRR", indicator);
         }
 
         [Fact]
         public void TestOneHourIndicatorString()
         {
-            Clock clock = new Clock(TimeSpan.Parse("13:17:02"));
+            Clock clock = new Clock(TimeSpan.Parse("05:00:00"));
             String indicator = clock.GetOneHourIndicatorString();
 
             // Test output String length
             Assert.Equal(4, indicator.Length);
 
-            // Test for one hour indicator string
-            Assert.Equal("RRRO", indicator);
+            // Test for one hour indicator string (all lamps off)
+            Assert.Equal("OOOO", indicator);
+
+            clock = new Clock(TimeSpan.Parse("14:00:00"));
+            indicator = clock.GetOneHourIndicatorString();
+
+            // Test output String length
+            Assert.Equal(4, indicator.Length);
+
+            // Test for one hour indicator string (all lamps on)
+            Assert.Equal("RRRR", indicator);
         }
 
         [Fact]
         public void TestFiveMinutesIndicatorString()
         {
-            Clock clock = new Clock(TimeSpan.Parse("13:17:02"));
+            Clock clock = new Clock(TimeSpan.Parse("00:00:00"));
             String indicator = clock.GetFiveMinutesIndicatorString();
 
             // Test output String length
             Assert.Equal(11, indicator.Length);
 
-            // Test for five minutes indicator string
-            Assert.Equal("YYROOOOOOOO", indicator);
+            // Test for five minutes indicator string (all lamps off)
+            Assert.Equal("OOOOOOOOOOO", indicator);
+
+            clock = new Clock(TimeSpan.Parse("00:59:00"));
+            indicator = clock.GetFiveMinutesIndicatorString();
+
+            // Test output String length
+            Assert.Equal(11, indicator.Length);
+
+            // Test for five minutes indicator string (all lamps on, every third must be red)
+            Assert.Equal("YYRYYRYYRYY", indicator);
         }
 
         [Fact]
         public void TestOneMinuteIndicatorString()
         {
-            Clock clock = new Clock(TimeSpan.Parse("13:17:02"));
+            Clock clock = new Clock(TimeSpan.Parse("00:15:00"));
             String indicator = clock.GetOneMinuteIndicatorString();
 
             // Test output String length
             Assert.Equal(4, indicator.Length);
 
-            // Test for one minute indicator string
-            Assert.Equal("YYOO", indicator);
+            // Test for one minute indicator string (all lamps off)
+            Assert.Equal("OOOO", indicator);
+
+            clock = new Clock(TimeSpan.Parse("00:19:00"));
+            indicator = clock.GetOneMinuteIndicatorString();
+
+            // Test output String length
+            Assert.Equal(4, indicator.Length);
+
+            // Test for one minute indicator string (all lamps on)
+            Assert.Equal("YYYY", indicator);
         }
     }
 }
